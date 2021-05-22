80a40a473ea69fa9afefa9310ec35f0a9d255ba4
ericcapiz <ericcapiz@gmail.com>
2021-05-22 18:08:52 -0500

    can create user

--- be/routes/auth.js
+++ be/routes/auth.js
@@ -1,7 +1,29 @@
 const router = require('express').Router();
+const User = require("../models/User");
+const bcrypt = require('bcrypt');
 
-router.get("/",(req,res) =>{
-    res.send("auth api");
+//Register
+router.post("/register",async (req,res) =>{
+    
+
+    try {
+        //hashing password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+        //create new user
+        const newUser = await new User({
+            username: req.body.username,
+            email:req.body.email,
+            password: hashedPassword,
+        })
+
+        //save user to db
+        const user = await newUser.save();
+        res.status(200).json(user)
+    } catch (error) {
+        console.log("can not create user",error)
+    }
 })
 
 module.exports = router;
