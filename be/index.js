5d99e944f93fe2eac8a2e2c779f787cffccac4b2
ericcapiz <ericcapiz@gmail.com>
2021-05-22 17:45:38 -0500

    user schema created

--- be/index.js
+++ be/index.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const userRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
 
 const app = express();
 dotenv.config();
@@ -10,6 +12,19 @@ dotenv.config();
 mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true,  useUnifiedTopology: true, useCreateIndex: true},() =>{
     console.log("Connected to MongoDB")
 })
+
+//Middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+//Routes
+app.get("/",(req,res)=>{
+    res.send("api up")
+})
+app.use("/api/users", userRoute)
+app.use("/api/auth", authRoute)
+
 app.listen(8800, () => {
     console.log("Running Server....")
 })
