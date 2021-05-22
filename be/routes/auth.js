78dbf1b35ba426964e11aead9f8f2775110e6ff7
ericcapiz <ericcapiz@gmail.com>
2021-05-22 18:19:06 -0500

    user can login

--- be/routes/auth.js
+++ be/routes/auth.js
@@ -26,4 +26,19 @@ router.post("/register",async (req,res) =>{
     }
 })
 
+//Login
+router.post("/login", async (req,res) => {
+    try {
+        const user = await User.findOne({email:req.body.email});
+        !user && res.status(404).json("User Not Found");
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        !validPassword && res.status(400).json("Wrong Pasword");
+
+        res.status(200).json(user)
+    } catch (error) {
+        console.log("login error", error)
+    }
+})
+
 module.exports = router;
