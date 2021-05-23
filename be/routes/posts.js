5df3ddd1177f470c292dc2dbb3571c8f1d4e1eaf
ericcapiz <ericcapiz@gmail.com>
2021-05-22 20:15:11 -0500

    posts can be deleted

--- be/routes/posts.js
+++ be/routes/posts.js
@@ -28,8 +28,22 @@ router.put("/:id", async (req,res) =>{
         res.status(500).json(error)
     }
 })
-//Delete Post
 
+
+//Delete Post
+router.delete("/:id", async (req,res) =>{
+    try {
+        const post = await Post.findById(req.params.id);
+        if(post.userId === req.body.userId){
+            await post.deleteOne();
+            res.status(200).json("post has been deleted")
+        }else{
+            res.status(403).json("Can't delete other users posts")
+        }   
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
 //Like Post
 
 //Get Post
