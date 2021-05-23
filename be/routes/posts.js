f12ba3164e8075115b46e765fddab3bafebdc781
ericcapiz <ericcapiz@gmail.com>
2021-05-22 20:12:32 -0500

    users can update posts

--- be/routes/posts.js
+++ be/routes/posts.js
@@ -12,8 +12,22 @@ router.post("/", async (req,res)=>{
         res.status(500).json(error);
     }
 })
-//Update Post
 
+
+//Update Post
+router.put("/:id", async (req,res) =>{
+    try {
+        const post = await Post.findById(req.params.id);
+        if(post.userId === req.body.userId){
+            await post.updateOne({$set:req.body});
+            res.status(200).json("post has been updated")
+        }else{
+            res.status(403).json("Can't update other users posts")
+        }   
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
 //Delete Post
 
 //Like Post
