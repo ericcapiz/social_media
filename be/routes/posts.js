cb226afdcaf48b57075d048bef3541bb99f8c168
ericcapiz <ericcapiz@gmail.com>
2021-05-22 20:59:02 -0500

    be done

--- be/routes/posts.js
+++ be/routes/posts.js
@@ -44,10 +44,50 @@ router.delete("/:id", async (req,res) =>{
         res.status(500).json(error)
     }
 })
+
+
+
 //Like Post
+router.put("/:id/like", async (req,res)=>{
+    try {
+        const post = await Post.findById(req.params.id);
+        if(!post.likes.includes(req.params.userId)){
+            await post.updateOne({$push:{likes: req.body.userId}});
+            res.status(200).json("Post has been liked!")
+        }else{
+            await post.updateOne({$pull:{likes:req.body.userId}});
+            res.status(200).json("Post has been disliked!")
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
 
 //Get Post
+router.get("/:id",async(req,res)=>{
+    try {
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
 
 //Get timeline Posts
+router.get("/timeline/all", async(req,res)=>{
+    try {
+        const currentUser = await User.findById(req.body.userId);
+        const userPosts = await Post.find({userId: currentUser._id})
+        const friendPost = await Promise.all(
+            currentUser.followings.map(friendId => {
+              return Post.find({userId:friendId})
+            })
+        );
+        res.json(userPosts.concat(...friendPost))
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
 
 module.exports = router;
