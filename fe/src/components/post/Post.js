994e5f0a465aededab2cb6d94c664931b6a2d007
ericcapiz <ericcapiz@gmail.com>
2021-05-24 11:43:43 -0500

    created component to share post

---
+++ fe/src/components/post/Post.js
@@ -0,0 +1,11 @@
+import './post.css'
+
+const Post = () => {
+    return (
+        <div>
+            post
+        </div>
+    )
+}
+
+export default Post
--- fe/src/components/share/Share.js
+++ fe/src/components/share/Share.js
@@ -1,3 +1,4 @@
+import {PermMedia, Room, Person, EmojiEmotions} from '@material-ui/icons';
 import './share.css'
 
 const Share = () => {
@@ -5,11 +6,30 @@ const Share = () => {
         <div className="share" >
             <div className="shareWrapper">
                 <div className="shareTop">
-                    <img src="/assests/person/3.jpeg" alt="" className="shareProfileImg" />
-                    <input placeholder="What's on your mind?" className="shareInput"></input>
+                    <img src="/assets/person/me.jpg" alt="" className="shareProfileImg" />
+                    <input placeholder="What's on your mind?" className="shareInput" />
                 </div>
+                <hr className="shareHr" />
                 <div className="shareBottom">
-                    
+                    <div className="shareOptions">
+                        <div className="shareOption">
+                            <PermMedia htmlColor="tomato" className="shareIcon" />
+                            <span className='shareOptionText'>Upload Media</span>
+                        </div>
+                        <div className="shareOption">
+                            <Person htmlColor="blue" className="shareIcon" />
+                            <span className='shareOptionText'>Tag Friend</span>
+                        </div>
+                        <div className="shareOption">
+                            <Room htmlColor="green" className="shareIcon" />
+                            <span className='shareOptionText'>Tag Location</span>
+                        </div>
+                        <div className="shareOption">
+                            <EmojiEmotions htmlColor="goldenrod" className="shareIcon" />
+                            <span className='shareOptionText'>Feeling</span>
+                        </div>
+                    </div>
+                    <button className="shareButton">Share</button>
                 </div>
             </div>
         </div>
