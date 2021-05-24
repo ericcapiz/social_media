ff14b983a901143f1bd9e8b121ace344399f3fd4
ericcapiz <ericcapiz@gmail.com>
2021-05-24 12:28:29 -0500

    layout complete

--- fe/src/components/rightbar/Rightbar.js
+++ fe/src/components/rightbar/Rightbar.js
@@ -2,8 +2,24 @@ import './rightbar.css'
 
 const Rightbar = () => {
     return (
-        <div className="right">
-            rightbar
+        <div className="rightbar">
+            <div className="rightbarWrapper">
+                <div className="birthdayContainer">
+                    <img src="assets/gift.png" alt="" className="birthdayImg" />
+                    <span className="birthdayText"><b>Mia</b> and <b>2 other friends</b> have a birthday coming up!</span>
+                </div>
+                <img src="/assets/ad.png" alt="" className="rightbarAd" />
+                <h4 className="rightbarTitle">Online Friends:</h4>
+                <ul className="rightbarFriendList">
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="assets/person/10.jpeg" alt="" className="rightbarProfileImg" />
+                            <span className="rightbarOnline"></span>
+                        </div>
+                        <span className="rightbarUsername">Stacy</span>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
