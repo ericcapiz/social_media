57fb39197ba27d96919a5381803f71694cd6570f
ericcapiz <ericcapiz@gmail.com>
2021-05-25 18:47:39 -0500

    login form updated

--- fe/src/components/pages/login/Login.js
+++ fe/src/components/pages/login/Login.js
@@ -1,6 +1,15 @@
 import './login.css';
+import {useRef} from 'react';
 
 const Login = () => {
+
+    //stop re-rendering page
+    const email = useRef();
+    const password = useRef();
+
+    const handleClick = (e) => {
+        e.prevent.default();
+    }
     return (
         <div className="login">
             <div className="loginWrapper">
@@ -9,13 +18,13 @@ const Login = () => {
                     <span className="loginDesc">Connect with friends and other devs around the world on Social-Devs.</span>
                 </div>
                 <div className="loginRight">
-                    <div className="loginBox">
-                        <input placeholder="Email" className="loginInput" />
-                        <input placeholder="Password" className="loginInput" />
+                    <form className="loginBox" onSubmit={handleClick} >
+                        <input required ref={email} type="email" placeholder="Email" className="loginInput" />
+                        <input required minLength='6' ref={password} type="password" placeholder="Password" className="loginInput" />
                         <button className="loginButton">Log-In</button>
                         <span className="loginForgot">Forgot Password?</span>
                         <button className="loginRegisterButton">Register New Account</button>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
